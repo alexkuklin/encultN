66330a11e5be838bc413d03c26cccddeb872a8ec
grahor-targ <grahor@gmail.com>
2016-02-11 21:38:36 +0300

    Трейсеры

--- app/redux/api.js
+++ app/redux/api.js
@@ -19,36 +19,44 @@ function fetchFromApi(url, fetchOptions = null) {
 }
 
 export function voteForAnswerPromise(id) {
+	console.log('voteForAnswerPromise');
   return fetchFromApi(`vote-for-answer?language=${getLang()}&answerIdentifier=${id}`, { method: 'put', credentials: 'include' });
 }
 export function fetchQuestionsPromise() {
   //return fetchFromApi(getLang() + '/questions.json');
+   console.log('fetchQuestionsPromise');
   return fetchFromApi('questions_list/' + getLang() + '.json');
   //questions_list/ru.json
 }
 export function fetchAnswersPromise(path) {
   //return fetchFromApi(getLang() + '/questions/' + path + '.json');
+  console.log('fetchAnswersPromise');
   return fetchFromApi('question/' + getLang() + '/' + path + '.json');
   //question/ru/48.json
 }
 export function fetchWorldviewsPromise() {
   //return fetchFromApi(getLang() + '/worldviews.json');
+  console.log('fetchWorldviewsPromise');
   return fetchFromApi('worldviews_list/'+getLang() + '.json');
 }
 export function fetchWorldviewPromise(path) {
   //return fetchFromApi(getLang() + '/worldviews/' + path + '.json');
+  console.log('fetchWorldviewPromise');
   return fetchFromApi('worldview/' +getLang() + '/' + path + '.json');
   //worldview/ru/46.json
 }
 export function fetchBlogsPromise() {
   //return fetchFromApi(getLang() + '/blogs.json');
+    console.log('fetchBlogsPromise');
   return fetchFromApi('blogs_list/' + getLang() + '.json');
   //blogs_list/en.json
 }
 export function fetchBlogPromise(path) {
+	console.log('fetchBlogPromise');
   return fetchFromApi(getLang() + '/blogs/' + path + '.json');
 }
 export function fetchAboutPromise() {
+	console.log('fetchAboutPromise');
   //return fetchFromApi(getLang() + '/about.json');
   return fetchFromApi('about/' +getLang() +'.json');
   //about/ru.json
